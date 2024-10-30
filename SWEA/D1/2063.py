605162bede9343a17641ea82302b000fc268c801
A-Yeong <dkdud064@kumoh.ac.kr>
2024-10-30 22:09:19 +0900

    FIX: 오류 수정

--- SWEA/D1/2063.py
+++ SWEA/D1/2063.py
@@ -1,8 +1,4 @@
 T = int(input())
-
-for test_case in range(1, T + 1):
-    data = list(map(int, input().split()))
-    data = sorted(data)
-    idx = T // 2
-
-    print(data[idx])
+data = list(map(int, input().split()))
+data = sorted(data)
+print(data[T // 2])
