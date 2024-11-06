f8f5a59a6670aec80450a89f0081432ce0f639e5
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-06 18:41:29 +0900

    FIX: SWEA D2 1959

--- SWEA/D2/1959.py
+++ SWEA/D2/1959.py
@@ -2,10 +2,24 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
-    Aarr = list(map(int, input().split()))
-    Barr = list(map(int, input().split()))
+    if N < M:
+        long, short = M, N
+        shortArr = list(map(int, input().split()))
+        longArr = list(map(int, input().split()))
+    else:
+        long, short = N, M
+        longArr = list(map(int, input().split()))
+        shortArr = list(map(int, input().split()))
+    
+    ans = 0
+    for i in range(long - short + 1):
+        sum = 0
+        for j in range(short):
+            sum += longArr[i + j] * shortArr[j]
+        if sum > ans:
+            ans = sum
 
-    print(Aarr, Barr)
+    print("#%d" %test_case, ans)
 
     # moreArr = []
     # lessArr = []
