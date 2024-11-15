7d27b1a67b7b1400bf71896565e70f3de841c6a8
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-15 22:35:28 +0900

    SWEA 1970 2회차

--- SWEA/D2/1970.py
+++ SWEA/D2/1970.py
@@ -1,33 +1,33 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     N = int(input())
-    moneyArr = [0, 0, 0, 0, 0, 0, 0, 0]
+    a = [0, 0, 0, 0, 0, 0, 0, 0]
 
     while(N >= 50000):
-            N -= 50000
-            moneyArr[0] += 1
-    while(N >= 10000):
-            N -= 10000
-            moneyArr[1] += 1
-    while(N >= 5000):
-            N -= 5000
-            moneyArr[2] += 1
-    while(N >= 1000):
-            N -= 1000
-            moneyArr[3] += 1
-    while(N >= 500):
-            N -= 500
-            moneyArr[4] += 1
-    while(N >= 100):
-            N -= 100
-            moneyArr[5] += 1
-    while(N >= 50):
-            N -= 50
-            moneyArr[6] += 1
-    while(N >= 10):
-            N -= 10
-            moneyArr[7] += 1
+        a[0] += 1
+        N -= 50000
+    while (N >= 10000):
+        a[1] += 1
+        N -= 10000
+    while (N >= 5000):
+        a[2] += 1
+        N -= 5000
+    while (N >= 1000):
+        a[3] += 1
+        N -= 1000
+    while (N >= 500):
+        a[4] += 1
+        N -= 500
+    while (N >= 100):
+        a[5] += 1
+        N -= 100
+    while (N >= 50):
+        a[6] += 1
+        N -= 50
+    while (N >= 10):
+        a[7] += 1
+        N -= 10
 
-    print("#%d" %test_case)
-    print(*moneyArr)
+    print("#%d" %tc)
+    print(*a)
