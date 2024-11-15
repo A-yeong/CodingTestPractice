78154bb58f5ea77cc8cee7ca366d54fe606e1850
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-15 17:49:21 +0900

    SWEA 1948 2회차

--- SWEA/D2/1948.py
+++ SWEA/D2/1948.py
@@ -1,12 +1,11 @@
 T = int(input())
+days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     m1, d1, m2, d2 = map(int, input().split())
-    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     start_day = sum(days[:m1]) + d1
     end_day = sum(days[:m2]) + d2
-
     ans = end_day - start_day + 1
 
-    print("#%d" %test_case, ans)
+    print("#%d" %tc, ans)
