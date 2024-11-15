4b45804fd661bbaaceb8dddba514fe4daeb2cbeb
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-15 18:23:53 +0900

    SWEA 1959 2회차

--- SWEA/D2/1959.py
+++ SWEA/D2/1959.py
@@ -1,39 +1,22 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    if N < M:
-        long, short = M, N
-        shortArr = list(map(int, input().split()))
-        longArr = list(map(int, input().split()))
+    short_arr, long_arr = [], []
+
+    if(N > M):
+        long_arr = list(map(int, input().split()))
+        short_arr = list(map(int, input().split()))
     else:
-        long, short = N, M
-        longArr = list(map(int, input().split()))
-        shortArr = list(map(int, input().split()))
-    
+        short_arr = list(map(int, input().split()))
+        long_arr = list(map(int, input().split()))
+
     ans = 0
-    for i in range(long - short + 1):
+    for i in range(len(long_arr) - len(short_arr) + 1):
         sum = 0
-        for j in range(short):
-            sum += longArr[i + j] * shortArr[j]
+        for j in range(len(short_arr)):
+            sum += long_arr[i + j] * short_arr[j]
         if sum > ans:
             ans = sum
 
-    print("#%d" %test_case, ans)
-
-    # moreArr = []
-    # lessArr = []
-    # if(len(Aarr) >= len(Barr)):
-    #     moreArr = Aarr
-    #     lessArr = Barr
-    # else:
-    #     moreArr = Barr
-    #     lessArr = Aarr
-
-    # print(moreArr, lessArr)
-
-    # max = 0
-    # for i in range(len(moreArr)):
-    #     sum = 0
-    #     for j in range(len(lessArr)):
-            
+    print("#%d" %tc, ans)
