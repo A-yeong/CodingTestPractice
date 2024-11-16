1fed650b7cf4273efe90317f9968ed1fc87f621a
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-16 18:50:50 +0900

    SWEA 1974 3회차

--- SWEA/D2/1974.py
+++ SWEA/D2/1974.py
@@ -3,31 +3,28 @@ T = int(input())
 for tc in range(1, T + 1):
     data = [list(map(int, input().split())) for _ in range(9)]
     one_ten = list(range(1, 10))
-    chk = 1
+    ans = 1
 
     for i in range(9):
-        if(chk == 0):
-            break
-
         # 가로
         if(sorted(data[i]) != one_ten):
-            chk = 0
+            ans = 0
 
         # 세로
-        yarr = []
+        sero_arr = []
         for j in range(9):
-            yarr.append(data[j][i])
-        if(sorted(yarr) != one_ten):
-            chk = 0
+            sero_arr.append(data[j][i])
+        if(sorted(sero_arr) != one_ten):
+            ans = 0
 
         # 3x3
         for j in range(9):
             if(i % 3 == 0 and j % 3 == 0):
-                sqrarr = []
+                sqr_arr = []
                 for k in range(3):
                     for l in range(3):
-                        sqrarr.append(data[i + k][j + l])
-                if(sorted(sqrarr) != one_ten):
-                    chk = 0
+                        sqr_arr.append(data[i+k][j+l])
+            if(sorted(sqr_arr) != one_ten):
+                ans = 0
 
-    print("#%d" %tc, chk)
+    print("#%d" %tc, ans)
