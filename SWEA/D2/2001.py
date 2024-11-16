0c0e4aef34f881348e31e592beec2e5e0af87b9d
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-16 15:22:37 +0900

    SWEA 2001 2회차

--- SWEA/D2/2001.py
+++ SWEA/D2/2001.py
@@ -1,18 +1,16 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    flies = []
+    data = [list(map(int, input().split())) for _ in range(N)]
+    areas = []
 
     for i in range(N - M + 1):
         for j in range(N - M + 1):
-            sum = 0
+            area = 0
             for k in range(M):
                 for l in range(M):
-                    sum += arr[i + k][j + l]
-            flies.append(sum)
+                    area += data[i + k][j + l]
+            areas.append(area)
 
-    print("#%d" %test_case, max(flies))
-    # print(max)
-        
+    print("#%d" %tc, max(areas))
