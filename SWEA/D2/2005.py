3a9edfc7978ee00b79d13b710bce161fbf15a01c
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-16 22:54:57 +0900

    SWEA 2005 3회차

--- SWEA/D2/2005.py
+++ SWEA/D2/2005.py
@@ -2,20 +2,20 @@ T = int(input())
 
 for tc in range(1, T + 1):
     N = int(input())
-    triangle = [[] for _ in range(N)]
+    arr = [[] for _ in range(N)]
 
     for i in range(N):
         if i == 0:
-            triangle[i].append(1)
+            arr[i].append(1)
         elif i == 1:
-            triangle[i].append(1)
-            triangle[i].append(1)
+            arr[i].append(1)
+            arr[i].append(1)
         else:
-            triangle[i].append(1)
+            arr[i].append(1)
             for j in range(1, i):
-                triangle[i].append(triangle[i-1][j-1] + triangle[i-1][j])
-            triangle[i].append(1)
+                arr[i].append(arr[i - 1][j - 1] + arr[i - 1][j])
+            arr[i].append(1)
 
     print("#%d" %tc)
     for i in range(N):
-        print(*triangle[i])
+        print(*arr[i])
