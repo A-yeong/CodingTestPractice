7f0529cb6759cc6ba6248cb2de69c0269b9bea3c
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-16 17:18:04 +0900

    SWEQ 1954 3회차

--- SWEA/D2/1954.py
+++ SWEA/D2/1954.py
@@ -2,17 +2,18 @@ T = int(input())
 
 for tc in range(1, T + 1):
     N = int(input())
-    arr = [[0 for _ in range(N)] for _ in range(N)]
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+    data = [[0 for _ in range(N)] for _ in range(N)]
+
+    dy = [0, 1, 0, -1]
+    dx = [1, 0, -1, 0]
 
     def snail(x, y, d, num):
-        arr[x][y] = num
+        data[y][x] = num
         nx = x + dx[d]
         ny = y + dy[d]
         if num == N * N:
             return
-        if nx < 0 or ny < 0 or nx >= N or ny >= N or arr[nx][ny] != 0:
+        if nx < 0 or ny < 0 or nx >= N or ny >= N or data[ny][nx] != 0:
             d = (d + 1) % 4
             snail(x, y, d, num)
         else:
@@ -21,4 +22,4 @@ for tc in range(1, T + 1):
     snail(0, 0, 0, 1)
     print("#%d" %tc)
     for i in range(N):
-        print(*arr[i])
+        print(*data[i])
