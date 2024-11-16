188f9edc3f34773d0fd7fdc356e898604aa7d846
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-16 14:57:38 +0900

    SWEA 1989 2회차

--- SWEA/D2/1989.py
+++ SWEA/D2/1989.py
@@ -1,10 +1,10 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
-    text = str(input())
-    reversText = text[::-1]
+for tc in range(1, T + 1):
+    data = input()
+    reverse_data = data[::-1]
 
-    if(text == reversText):
-        print("#%d" %test_case, 1)
+    if data == reverse_data:
+        print("#%d" %tc, 1)
     else:
-        print("#%d" %test_case, 0)
+        print("#%d" %tc, 0)
