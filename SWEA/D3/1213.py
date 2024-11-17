2f8a40f054061569f5e86e9a0b1a42bc0d84c972
A-Yeong <dkdud064@kumoh.ac.kr>
2024-11-17 13:12:17 +0900

    SWEA 1213 2회차

--- SWEA/D3/1213.py
+++ SWEA/D3/1213.py
@@ -1,12 +1,8 @@
 T = 10
 
-for test_case in range(1, T + 1):
-    n = int(input())
+for tc in range(1, T + 1):
+    t = int(input())
     search_str = input()
     input_str = input()
-    cnt = 0
 
-    for i in range(0, len(input_str) - len(search_str) + 1):
-        if(input_str[i:i+len(search_str)] == search_str):
-            cnt += 1
-    print("#%d" %test_case, cnt)
+    print("#%d" %tc, input_str.count(search_str))
