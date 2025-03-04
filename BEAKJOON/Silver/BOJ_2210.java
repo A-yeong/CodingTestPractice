d2218d7001e206265ef03b27dd6993582f60dbeb
A-Yeong <dkdud064@kumoh.ac.kr>
2025-03-04 13:56:22 +0900

    BOJ 2210

---
+++ BEAKJOON/Silver/BOJ_2210.java
@@ -0,0 +1,48 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_2210 {
+    static int[][] digits = new int[5][5];
+    static Set<String> numbers = new HashSet<>();
+    static int[] dx = new int[]{-1, 0, 1, 0};
+    static int[] dy = new int[]{0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                digits[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                dfs(i, j, 0, "");
+            }
+        }
+
+        System.out.println(numbers.size());
+    }
+
+    private static void dfs(int x, int y, int depth, String num){
+        if(depth == 6) {
+            numbers.add(num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
+                dfs(nx, ny, depth + 1, num + digits[nx][ny]);
+            }
+        }
+    }
+}
